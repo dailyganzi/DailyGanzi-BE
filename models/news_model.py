b602a198ebec33fd8dbbca88ce36a357a6cb59c9
nanyoung00 <nyungnim00@gmail.com>
2023-08-12 05:07:46 +0900

    pydantic news 모델 생성(MongoDB 연동x, ObjectID 부분 임시로 적어둠)

--- models/news_model.py
+++ models/news_model.py
@@ -1,2 +1,24 @@
-from pydantic import BaseModel
+from typing import List, Dict
+from pydantic import BaseModel, HttpUrl, Field
+from bson import ObjectId
+
+
+class NewsDetail(BaseModel):
+    img_url: HttpUrl
+    content: List[str]
+    articles_url: List[HttpUrl]
+
+
+class CategoryDetail(BaseModel):
+    title1: List[NewsDetail]
+    title2: List[NewsDetail]
+    title3: List[NewsDetail]
+
+
+class NewsArticle(BaseModel):
+    _id: ObjectId = Field(..., alias="_id")
+    updated: str
+    category_name: str
+    category_id: int
+    details: List[CategoryDetail]
 
